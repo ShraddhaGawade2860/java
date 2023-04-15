a732a783d3d3df904766ab29d9274832d143be85
Shraddha Gawade <64028089+ShraddhaGawade2860@users.noreply.github.com>
2023-04-15 14:05:33 +0530

    Update Voting.java

--- Voting.java
+++ Voting.java
@@ -6,8 +6,8 @@ public static void main(String[] args)
 {
 int age,shrt;
 Scanner sc = new Scanner(System.in);
-age = sc.nextInt();
 System.out.println("Enter the age:");
+age = sc.nextInt();
 if(age>=18)
 {
 System.out.println("You are old enough to vote for national election");
