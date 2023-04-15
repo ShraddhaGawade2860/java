ba175b80e8d6d3d1769a76c37a409429e13d0ee8
Shraddha Gawade <64028089+ShraddhaGawade2860@users.noreply.github.com>
2023-04-15 14:01:41 +0530

    Update Multiply.java

--- Multiply.java
+++ Multiply.java
@@ -1,15 +1,14 @@
 import java.util.Scanner;
-import java.util.*;
 public class Multiply
- {
+{
 public static void main(String[] args) 
 {
-Scanner sc=new Scanner(System.in);
-int num = sc.nextInt();
-System.out.println("Enter the number:");
-for(int i = 1; i <= 10; ++i)
+Scanner s = new Scanner(System.in);
+System.out.print("Enter number:");        
+int n=s.nextInt();
+for(int i=1; i <= 10; i++)
 {
-System.out.println("%d * %d = %d \n", num, i, num * i);
+System.out.println(n+" * "+i+" = "+n*i);
 }
 }
 }
